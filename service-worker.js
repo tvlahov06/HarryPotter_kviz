6b7488230340030a8f01354aee6632d0b191b1ee
Tomislav <tomislav.v6@gmail.com>
2025-07-07 02:24:13 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("hp-kviz").then((cache) =>
+      cache.addAll(["./", "./index.html", "./manifest.json"])
+    )
+  );
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
