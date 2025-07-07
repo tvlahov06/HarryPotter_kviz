c9dae018581ce8b724d5ec624f1c640bd914dd25
Tomislav <tomislav.v6@gmail.com>
2025-07-07 02:40:57 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,21 @@ self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("hp-kviz").then((cache) =>
       cache.addAll(["./", "./index.html", "./manifest.json"])
-    )
+  return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "service-worker.js",
+        "icon-192.png",
+        "icon-512.png"
+      ]);
+    })
   );
 });
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
